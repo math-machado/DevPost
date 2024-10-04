8235b7c42b8fe2a2a8fbaf1a984b66094385afae
math-machado <matheus.machado2308@outlook.com>
2024-10-04 09:44:45 -0300

    hometab

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,14 +1,53 @@
 import { createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import Feather from 'react-native-vector-icons/Feather'
 
 import Home from '../pages/Home'
 import Profile from '../pages/Profile'
 import Search from '../pages/Search'
-//import NewPost from '../pages/NewPost'
-//import PostsUser from '../pages/PostsUser'
+import NewPost from '../pages/NewPost'
+import PostsUser from '../pages/PostsUser'
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function StackRoutes(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen
+            name='Home'
+            component={Home}
+            options={{
+                headerShown: false
+            }}
+            />
+            
+            <Stack.Screen
+            name='NewPost'
+            component={NewPost}
+            options={{
+                title: 'Novo Post',
+                headerTintColor: '#FFF',
+                headerStyle:{
+                    backgroundColor: '#36393F'
+                }
+            }}
+            />
+
+            <Stack.Screen
+            name='PostsUser'
+            component={PostsUser}
+            options={{
+                headerTintColor: '#FFF',
+                headerStyle:{
+                    backgroundColor: '#36393F'
+                }
+            }}
+            />
+        </Stack.Navigator>
+    )
+}
 
 function AppRoutes(){
     return(
@@ -25,8 +64,8 @@ function AppRoutes(){
             }
         }}>
             <Tab.Screen 
-            name='Home' 
-            component={Home}
+            name='HomeTab' 
+            component={StackRoutes}
             options={{
                 tabBarIcon: ({color, size}) => {
                     return(
