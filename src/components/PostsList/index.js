e420ac7c1684c0deb1705a621edb978680a2898e
math-machado <matheus.machado2308@outlook.com>
2024-10-13 09:55:42 -0300

    buscando posts do usuario

--- src/components/PostsList/index.js
+++ src/components/PostsList/index.js
@@ -12,13 +12,14 @@ import { useState } from 'react';
 
 import firestore from '@react-native-firebase/firestore'
 
+import { useNavigation } from '@react-navigation/native';
+
 export default function PostsList({data, userId}) { 
+
+    const navigation = useNavigation();
 
     const [likePost, setLikePost] = useState(data?.likes)
     const [myLike, setMyLike] = useState(false)
-    
-    console.log(userId);
-    
 
     function formatTimePost(){
         //console.log(new Date(data.created.seconds * 1000));
@@ -34,48 +35,48 @@ export default function PostsList({data, userId}) {
         )
     };
 
-   async function handleLikePost(id, likes){
-    const docId = `${userId}_${id}`
+    async function handleLikePost(id, likes){
+        const docId = `${userId}_${id}`
 
-    const doc = await firestore().collection('likes')
-    .doc(docId).get();
+        const doc = await firestore().collection('likes')
+        .doc(docId).get();
 
-    if(doc.exists){
+        if(doc.exists){
+            await firestore().collection('posts')
+            .doc(id).update({
+                likes: likes - 1
+            })
+
+            await firestore().collection('likes').doc(docId)
+            .delete()
+            .then(() => {
+                setLikePost(likes - 1)
+                setMyLike(false)
+            })
+
+            return;
+        }
+
+        await firestore().collection('likes')
+        .doc(docId).set({
+            postId: id,
+            userId: userId
+        })
+
         await firestore().collection('posts')
         .doc(id).update({
-            likes: likes - 1
+            likes: likes + 1
         })
-
-        await firestore().collection('likes').doc(docId)
-        .delete()
         .then(() => {
-            setLikePost(likes - 1)
-            setMyLike(false)
+            setLikePost(likes + 1)
+            setMyLike(true)
         })
 
-        return;
     }
-
-    await firestore().collection('likes')
-    .doc(docId).set({
-        postId: id,
-        userId: userId
-    })
-
-    await firestore().collection('posts')
-    .doc(id).update({
-        likes: likes + 1
-    })
-    .then(() => {
-        setLikePost(likes + 1)
-        setMyLike(true)
-    })
-
-   }
     
     return (
     <Container>
-        <Header>
+        <Header onPress={() => navigation.navigate('PostsUser', { title: data.autor, userId: data.userid})}>
             {data.avatarUrl ? (
                  <Avatar source={{ uri: data.avatarUrl }}/>
             ) : (
--- src/pages/PostsUser/index.js
+++ src/pages/PostsUser/index.js
@@ -1,9 +1,65 @@
 import { Text, View } from 'react-native';
 
+import { useRoute, useNavigation, useFocusEffect } from '@react-navigation/native';
+
+import { useCallback, useLayoutEffect, useState } from 'react';
+
+import firestore from '@react-native-firebase/firestore'
+
 export default function PostsUser() {
- return (
+
+  const route = useRoute();
+  const navigation = useNavigation();
+
+  const [title, setTitle] = useState(route.params?.title)
+
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: title === '' ? '' : title
+    })
+  }, [navigation, title]);
+
+
+  useFocusEffect(
+    useCallback(() => {
+
+      isActive = true
+
+      firestore().collection('posts')
+      .where('userid', '==', route.params?.userId)
+      .orderBy('created', 'desc')
+      .get()
+      .then((snapshot) => {
+        let data = []
+        snapshot.docs.map( u => {
+          data.push({
+            ...u.data(),
+            id: u.id
+          })
+        })
+
+        if(isActive){
+          setPosts(data)
+          console.log(data);
+          setLoading(false)
+        }
+        
+      })
+
+
+      return () => {
+        isActive = false
+      }
+
+    }, [])
+  )
+
+  return (
    <View>
-    <Text>Tela PostsUser</Text>
+    <Text>{title}</Text>
    </View>
   );
 };
