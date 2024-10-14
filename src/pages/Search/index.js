1c332e26c0e00aff8ec4f302fecae7985b602d02
math-machado <matheus.machado2308@outlook.com>
2024-10-14 11:06:17 -0300

    buscando usuarios

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -3,12 +3,39 @@ import { Text, View } from 'react-native';
 import { Container, AreaInput, Input, List } from './style'
 
 import Feather from 'react-native-vector-icons/Feather'
-import { useState } from 'react';
+
+import { useState, useEffect } from 'react';
+
+import firestore from '@react-native-firebase/firestore'
 
 export default function Search() {
 
   const [input, setInput] = useState('')
   const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    if(input === '' || input === undefined){
+      setUsers([])
+      return;
+    }
+
+    firestore().collection('users')
+    .where('nome', '>=', input)
+    .where('nome', '<=', input + "\uf8ff")
+    .onSnapshot((snapshot) => {
+      const listUsers = []
+
+      snapshot.forEach((doc) => {
+        listUsers.push({
+          ...doc.data(),
+          id: doc.id
+        })
+      })
+
+      setUsers(listUsers)
+      
+    })
+  }, [input])
   
   return (
    <Container>
